967846bb530005e6e5c1069cdaea103e1dde04bf
Juan Castro <jecastro2@uc.cl>
2017-04-20 15:09:30 -0300

    agregada gema annotate

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  username   :string
+#  role       :string
+#  email      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class User < ApplicationRecord
+  validates :name, presence: true
 end
