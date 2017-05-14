2f2346100d7db00af7546ed12c1d1fa65d8ea707
Juan Castro <jecastro2@uc.cl>
2017-05-14 14:31:40 -0400

    creado dropdown menu

--- app/assets/javascripts/layout.js
+++ app/assets/javascripts/layout.js
@@ -1,6 +1,10 @@
-// $('nav a').click(function() {
-//   var $jumbo = $('.jumbotron');
-//   var $header = $('header');
-//
-//   $jumbo.css('margin-top', ($header.height() + 10) + "px");
-// })
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+    const myDropdown = $("#myDropdown");
+      if (myDropdown.hasClass('show')) {
+        myDropdown.removeClass('show');
+      }
+  } else if (e.target.matches('.dropbtn')) {
+    $("#myDropdown").toggleClass("show");
+  }
+}
