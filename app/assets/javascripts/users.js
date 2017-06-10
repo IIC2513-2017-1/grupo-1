6b8c6b03f875d9aa1b892a40260cf549e3738a89
Juan Castro <jecastro2@uc.cl>
2017-06-10 15:59:54 -0400

    agregado tab en perfil de usuarios

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,1 +1,18 @@
+$(document).on('turbolinks:load', function () {
+  var $tabs = $('.tablinks');
+  $tabs.on('click', function(e){
+    var $link = $(this);
+    var $tab = $('.' + $link.data('tab'));
 
+    $('.tab-content').each(function(){
+      $(this).hide();
+    });
+
+    $tabs.each(function(){
+      $(this).removeClass('active');
+    });
+
+    $link.addClass('active');
+    $tab.show();
+  });
+});
